0732194f7d5148d91004613a2c139175ec65af4e
Max Schik <max.schik@googlemail.com>
2023-12-26 23:04:00 +0100

    fixes dotfiles install

--- src/my-dotfiles/install.sh
+++ src/my-dotfiles/install.sh
@@ -57,6 +57,44 @@ else
     exit 1
 fi
 
+# If in automatic mode, determine if a user already exists, if not use vscode
+if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+    if [ "${_REMOTE_USER}" != "root" ]; then
+        USERNAME="${_REMOTE_USER}"
+    else
+        USERNAME=""
+        POSSIBLE_USERS=("devcontainer" "vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+        for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+            if id -u ${CURRENT_USER} > /dev/null 2>&1; then
+                USERNAME=${CURRENT_USER}
+                break
+            fi
+        done
+        if [ "${USERNAME}" = "" ]; then
+            USERNAME=vscode
+        fi
+    fi
+elif [ "${USERNAME}" = "none" ]; then
+    USERNAME=root
+fi
+
+# *********************************
+# ** Shell customization section **
+# *********************************
+
+if [ "${USERNAME}" = "root" ]; then
+    user_home="/root"
+# Check if user already has a home directory other than /home/${USERNAME}
+elif [ "/home/${USERNAME}" != $( getent passwd $USERNAME | cut -d: -f6 ) ]; then
+    user_home=$( getent passwd $USERNAME | cut -d: -f6 )
+else
+    user_home="/home/${USERNAME}"
+    if [ ! -d "${user_home}" ]; then
+        mkdir -p "${user_home}"
+        chown ${USERNAME}:${group_name} "${user_home}"
+    fi
+fi
+
 # Install packages for appropriate OS
 case "${ADJUSTED_ID}" in
     "debian")
