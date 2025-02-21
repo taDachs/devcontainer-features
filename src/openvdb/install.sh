cb1b366e0155b757c92f334f79f6c7ff1ac6621e
Max Schik <max.schik@googlemail.com>
2025-02-21 20:45:47 +0100

    missing c++ compiler

--- src/openvdb/install.sh
+++ src/openvdb/install.sh
@@ -5,7 +5,7 @@ install_on_debian() {
   export DEBIAN_FRONTEND=noninteractive
 
   apt-get update -y
-  apt-get install -y libboost-iostreams-dev libblosc-dev libtbb-dev cmake git
+  apt-get install -y libboost-iostreams-dev libblosc-dev libtbb-dev cmake git build-essential g++
   rm -rf /var/lib/apt/lists/*
 
   cd /tmp
